6dfb3215679668b2d6a084081014ece6ad5e07f1
dhoat30 <dhoat30@gmail.com>
2022-09-26 21:41:38 +1300

    fixing home page images

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'http://localhost:3000' || 'https://gonogo.co.nz',
+    siteUrl: process.env.SITE_URL || 'http://localhost:3000',
         // siteUrl: process.env.SITE_URL || 'http://localhost:3000',
     generateRobotsTxt: true
 }
